6fdf8776d710e6074ac65b1ef3a77c441f0103a9
John <john@Johns-MacBook-Pro.local>
2021-07-08 13:30:51 +0300

    1532 problem E

--- 1532/e/main.kt
+++ 1532/e/main.kt
@@ -1,15 +1,35 @@
-// https://codeforces.com/contest/1532/problem/D
+// https://codeforces.com/contest/1532/problem/E
 
 fun main() {
     readInt()
-    println(makeTeam(readInts()))
+    val list = readLongs()
+    val result = findBeautiful(list)
+    println(result.size)
+    println(result.joinToString(" "))
+
 }
 
 private fun readLn() = readLine()!! // string line
 private fun readInt() = readLn().toInt() // single int
 private fun readStrings() = readLn().split(" ") // list of strings
-private fun readInts() = readStrings().map { it.toInt() } // list of ints
+private fun readLongs() = readStrings().map { it.toLong() } // list of ints
 
-fun makeTeam(ranks: List<Int>): Int {
-    return ranks.sorted().chunked(2).sumOf{it -> (it[1] - it[0])}
+fun findBeautiful(input: List<Long>): List<Long> {
+    val sum = input.sum()
+    val list = input.mapIndexed { i, it -> Pair(it, i.toLong()) }.sortedBy { it.first }.toMutableList()
+    val beautifuls = mutableListOf<Long>()
+
+    for (i in 1..list.size-1) {
+        val current = list[i - 1]
+        if (list.last().first == sum - list.last().first - current.first) {
+            beautifuls.add(current.second + 1)
+        }
+    }
+    if (list.size > 2) {
+        val penultimate = list[list.size - 2]
+        if (penultimate.first == sum - list.last().first - penultimate.first) {
+            beautifuls.add(list.last().second + 1)
+        }
+    }
+    return beautifuls.toList()
 }
